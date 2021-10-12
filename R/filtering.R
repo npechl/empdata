d924d0a171ce1bb8f2fc5ff971e71816458a8da5
unknown <nikosp41@gmail.com>
2021-10-12 13:13:55 +0300

    update filtering

--- R/filtering.R
+++ R/filtering.R
@@ -25,7 +25,7 @@ filter_country <- function(emp.obj, country.name){
 #'
 #' @examples
 
-filter_envo <- function(emp.obj, envo.id, envo.level) {
+filter_envo <- function(emp.obj, envo.level, envo.id) {
 
 
 
@@ -44,21 +44,61 @@ filter_envo <- function(emp.obj, envo.id, envo.level) {
 #'
 #' @examples
 
-filter_empo <- function(emp.obj, empo.id, empo.level) {
+filter_empo <- function(emp.obj, empo.level, empo.id) {
 
 
 
 
 }
 
-filter_host_taxonomy <- function() {
+#' Title
+#'
+#' @param emp.obj
+#' @param superkingdom
+#' @param kingdom
+#' @param phylum
+#' @param class
+#' @param order
+#' @param family
+#' @param genus
+#' @param species
+#'
+#' @return
+#' @export
+#'
+#' @examples
+filter_host_taxonomy <- function(emp.obj,
+                                 superkingdom = NULL,
+                                 kingdom = NULL,
+                                 phylum = NULL,
+                                 class = NULL,
+                                 order = NULL,
+                                 family = NULL,
+                                 genus = NULL,
+                                 species = NULL) {
 
 
 
 
 }
 
-filter_collection_timestamp <- function() {
+#' Title
+#'
+#' @param emp.obj
+#' @param time.stamp
+#' @param year
+#' @param month
+#' @param day
+#'
+#' @return
+#' @export
+#'
+#' @examples
+filter_collection_timestamp <- function(emp.obj,
+                                        time.stamp = NULL,
+                                        year = NULL,
+                                        month = NULL,
+                                        day = NULL) {
 
 
 
